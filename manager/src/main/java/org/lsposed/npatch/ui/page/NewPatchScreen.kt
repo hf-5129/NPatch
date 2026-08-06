cc70d06640989050787e43d9ac3287236fa87fea
alpha_1425 <shibeta1206@outlook.com>
2026-08-06 16:28:18 +0800

    Fix padding in Row components on NewPatchScreen
    Test whether the error is caused by the negative sign

--- manager/src/main/java/org/lsposed/npatch/ui/page/NewPatchScreen.kt
+++ manager/src/main/java/org/lsposed/npatch/ui/page/NewPatchScreen.kt
@@ -508,7 +508,7 @@ private fun DoPatchBody(modifier: Modifier, navigator: DestinationsNavigator) {
                 Row(
                     Modifier
                         .fillMaxWidth()
-                        .padding(start = 24.dp, end = 24.dp, top = -4.dp, bottom = 24.dp)
+                        .padding(start = 24.dp, end = 24.dp, top = 0.dp, bottom = 24.dp)
                 ) {
                     Button(
                         modifier = Modifier.weight(1f),
@@ -530,7 +530,7 @@ private fun DoPatchBody(modifier: Modifier, navigator: DestinationsNavigator) {
                 Row(
                     Modifier
                         .fillMaxWidth()
-                        .padding(start = 24.dp, end = 24.dp, top = -4.dp, bottom = 24.dp)
+                        .padding(start = 24.dp, end = 24.dp, top = 0.dp, bottom = 24.dp)
                 ) {
                     Button(
                         modifier = Modifier.weight(1f),
